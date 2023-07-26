091d03b08b201221362566c10194a4f956dc3237
Tim Harding <tim@timharding.co>
2023-07-25 21:09:11 -0700

    Initial glyph rendering attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{fs::File, io::BufWriter, path::Path};
-
 use png::Encoder;
+use rusttype::{point, Font, Scale};
+use std::{
+    fs::{self, File},
+    io::BufWriter,
+    path::Path,
+};
 
 fn main() {
     let path = Path::new(r"/home/tim/temp.png");
@@ -19,6 +23,35 @@ fn main() {
             data[p] = x as u8;
             data[p + 1] = y as u8;
         }
+    }
+
+    let font_path = Path::new(r"/usr/share/fonts/TTF/CaskaydiaCoveNerdFont-Regular.ttf");
+    let font_data = fs::read(&font_path).unwrap();
+    let font = Font::try_from_vec(font_data).unwrap();
+    let height = 12.4f32;
+    let pixel_height = height.ceil() as usize;
+
+    let scale = Scale {
+        x: height * 2.0,
+        y: height,
+    };
+
+    // The ascent is the highest point of any glyph. We shift down so that first line doesn't clip.
+    let v_metrics = font.v_metrics(scale);
+    let offset = point(0.0, v_metrics.ascent);
+
+    let glyphs: Vec<_> = font.layout("rusttype", scale, offset).collect();
+    let last = glyphs.last().unwrap();
+    let width = last.position().x as f32 + last.unpositioned().h_metrics().advance_width;
+    let width = width.ceil() as usize;
+
+    println!("Width: {width}, height: {pixel_height}");
+    for glyph in glyphs {
+        glyph.draw(|x, y, v| {
+            let p = y as usize * 512 + x as usize;
+            data[p] = ((data[p] as f32) * v) as u8;
+            data[p + 1] = ((data[p + 1] as f32) * v) as u8;
+        });
     }
 
     w.write_image_data(&data).unwrap();
