d67ee71c0506cc90ed5097422ad696aa25688caf
Tim Harding <tim@timharding.co>
2023-07-25 20:45:02 -0700

    Initial image data writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{fs::File, io::BufWriter, path::Path};
+
+use png::Encoder;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(r"/home/tim/temp.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = Encoder::new(w, 512, 256);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut w = encoder.write_header().unwrap();
+
+    let mut data = [0u8; 256 * 512 * 3];
+    for x in 0..512 {
+        for y in 0..256 {
+            let p = (y * 512 + x) * 3;
+            data[p] = x as u8;
+            data[p + 1] = y as u8;
+        }
+    }
+
+    w.write_image_data(&data).unwrap();
 }
