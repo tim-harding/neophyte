4d9bc2ffd51f1a357a0cd577f6699ab7fecaa595
Tim Harding <tim@timharding.co>
2023-07-27 13:02:40 -0700

    Cleaned up option_set parsing

--- src/event/option_set.rs
+++ src/event/option_set.rs
@@ -1,4 +1,4 @@
-use super::util::{parse_bool, parse_string, parse_u64};
+use super::util::{parse_array, parse_bool, parse_string, parse_u64};
 use nvim_rs::Value;
 use std::vec::IntoIter;
 
@@ -35,95 +35,45 @@ impl TryFrom<IntoIter<Value>> for OptionSet {
     type Error = OptionSetParseError;
 
     fn try_from(values: IntoIter<Value>) -> Result<Self, Self::Error> {
-        let mut out = Self::default();
-        for value in values {
-            match value {
-                Value::Array(array) => {
-                    let mut iter = array.into_iter();
-                    let name = parse_string(iter.next().ok_or(OptionSetParseError)?)
-                        .ok_or(OptionSetParseError)?;
-                    let value = iter.next().ok_or(OptionSetParseError)?;
-                    match name.as_str() {
-                        "arabicshape" => {
-                            out.arabicshape = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ambiwidth" => {
-                            out.ambiwidth = Some(parse_string(value).ok_or(OptionSetParseError)?)
-                        }
-                        "emoji" => out.emoji = Some(parse_bool(value).ok_or(OptionSetParseError)?),
-                        "guifont" => {
-                            out.guifont = Some(parse_string(value).ok_or(OptionSetParseError)?)
-                        }
-                        "guifontwide" => {
-                            out.guifontwide = Some(parse_string(value).ok_or(OptionSetParseError)?)
-                        }
-                        "linespace" => {
-                            out.linespace = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "mousefocus" => {
-                            out.mousefocus = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "mousemoveevent" => {
-                            out.mousemoveevent = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "pumblend" => {
-                            out.pumblend = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "showtabline" => {
-                            out.showtabline = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "termguicolors" => {
-                            out.termguicolors = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_cmdline" => {
-                            out.ext_cmdline = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_hlstate" => {
-                            out.ext_hlstate = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_linegrid" => {
-                            out.ext_linegrid = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_messages" => {
-                            out.ext_messages = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_multigrid" => {
-                            out.ext_multigrid = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_popupmenu" => {
-                            out.ext_popupmenu = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_tabline" => {
-                            out.ext_tabline = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "ext_termcolors" => {
-                            out.ext_termcolors = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "term_name" => {
-                            out.term_name = Some(parse_string(value).ok_or(OptionSetParseError)?)
-                        }
-                        "term_colors" => {
-                            out.term_colors = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "term_background" => {
-                            out.term_background = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "stdin_fd" => {
-                            out.stdin_fd = Some(parse_u64(value).ok_or(OptionSetParseError)?)
-                        }
-                        "stdin_tty" => {
-                            out.stdin_tty = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        "stdout_tty" => {
-                            out.stdout_tty = Some(parse_bool(value).ok_or(OptionSetParseError)?)
-                        }
-                        _ => eprintln!("Unknown option: {name}"),
-                    }
+        let inner = move || -> Option<Self> {
+            let mut out = Self::default();
+            for value in values {
+                let array = parse_array(value)?;
+                let mut iter = array.into_iter();
+                let option_name = parse_string(iter.next()?)?;
+                let option_value = iter.next()?;
+                match option_name.as_str() {
+                    "arabicshape" => out.arabicshape = Some(parse_bool(option_value)?),
+                    "ambiwidth" => out.ambiwidth = Some(parse_string(option_value)?),
+                    "emoji" => out.emoji = Some(parse_bool(option_value)?),
+                    "guifont" => out.guifont = Some(parse_string(option_value)?),
+                    "guifontwide" => out.guifontwide = Some(parse_string(option_value)?),
+                    "linespace" => out.linespace = Some(parse_u64(option_value)?),
+                    "mousefocus" => out.mousefocus = Some(parse_bool(option_value)?),
+                    "mousemoveevent" => out.mousemoveevent = Some(parse_bool(option_value)?),
+                    "pumblend" => out.pumblend = Some(parse_u64(option_value)?),
+                    "showtabline" => out.showtabline = Some(parse_u64(option_value)?),
+                    "termguicolors" => out.termguicolors = Some(parse_bool(option_value)?),
+                    "ext_cmdline" => out.ext_cmdline = Some(parse_bool(option_value)?),
+                    "ext_hlstate" => out.ext_hlstate = Some(parse_bool(option_value)?),
+                    "ext_linegrid" => out.ext_linegrid = Some(parse_bool(option_value)?),
+                    "ext_messages" => out.ext_messages = Some(parse_bool(option_value)?),
+                    "ext_multigrid" => out.ext_multigrid = Some(parse_bool(option_value)?),
+                    "ext_popupmenu" => out.ext_popupmenu = Some(parse_bool(option_value)?),
+                    "ext_tabline" => out.ext_tabline = Some(parse_bool(option_value)?),
+                    "ext_termcolors" => out.ext_termcolors = Some(parse_bool(option_value)?),
+                    "term_name" => out.term_name = Some(parse_string(option_value)?),
+                    "term_colors" => out.term_colors = Some(parse_u64(option_value)?),
+                    "term_background" => out.term_background = Some(parse_u64(option_value)?),
+                    "stdin_fd" => out.stdin_fd = Some(parse_u64(option_value)?),
+                    "stdin_tty" => out.stdin_tty = Some(parse_bool(option_value)?),
+                    "stdout_tty" => out.stdout_tty = Some(parse_bool(option_value)?),
+                    _ => eprintln!("Unknown option: {option_name}"),
                 }
-                _ => return Err(OptionSetParseError),
             }
-        }
-        Ok(out)
+            Some(out)
+        };
+        inner().ok_or(OptionSetParseError)
     }
 }
 
