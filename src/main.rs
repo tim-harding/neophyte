96243a339a71544a8e784bfbab09c59901d95574
Tim Harding <tim@timharding.co>
2023-07-26 17:20:34 -0700

    Parsing grid resize events

--- src/main.rs
+++ src/main.rs
@@ -31,11 +31,14 @@ impl Handler for NeovimHandler {
     async fn handle_notify(&self, name: String, args: Vec<Value>, _neovim: Neovim<Self::Writer>) {
         println!("Notify: {name}");
         for arg in args {
-            println!();
-            println!("{arg:?}");
+            match Event::try_from(arg) {
+                Ok(event) => println!("{event:?}"),
+                Err(e) => match e {
+                    EventParseError::UnknownEvent => eprintln!("Unknown event: {name}"),
+                    _ => eprintln!("{e}"),
+                },
+            }
         }
-        println!();
-        println!();
         println!();
     }
 }
@@ -122,3 +125,85 @@ async fn main() {
 
     w.write_image_data(&data).unwrap();
 }
+
+#[derive(Debug, Clone, Copy)]
+enum Event {
+    GridResize(GridResize),
+}
+
+impl TryFrom<Value> for Event {
+    type Error = EventParseError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Array(array) => {
+                let mut iter = array.into_iter();
+                let event_name = iter.next().ok_or(EventParseError::Malformed)?;
+                let args = iter.next().ok_or(EventParseError::Malformed)?;
+                if !iter.next().is_none() {
+                    return Err(EventParseError::Malformed);
+                }
+                match event_name {
+                    Value::String(s) => match s.as_str() {
+                        Some(s) => match s {
+                            "grid_resize" => Ok(Self::GridResize(GridResize::try_from(args)?)),
+                            _ => Err(EventParseError::UnknownEvent),
+                        },
+                        None => Err(EventParseError::Malformed),
+                    },
+                    _ => Err(EventParseError::Malformed),
+                }
+            }
+            _ => Err(EventParseError::Malformed),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, thiserror::Error)]
+enum EventParseError {
+    #[error("Event is malformed")]
+    Malformed,
+    #[error("Received an unrecognized event name")]
+    UnknownEvent,
+    #[error("{0}")]
+    GridResize(#[from] GridResizeParseError),
+}
+
+#[derive(Debug, Clone, Copy)]
+struct GridResize {
+    pub grid: u64,
+    pub width: u64,
+    pub height: u64,
+}
+
+impl TryFrom<Value> for GridResize {
+    type Error = GridResizeParseError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Array(array) => {
+                let mut iter = array.into_iter().map(pos_int).flatten();
+                let grid = iter.next().ok_or(GridResizeParseError)?;
+                let width = iter.next().ok_or(GridResizeParseError)?;
+                let height = iter.next().ok_or(GridResizeParseError)?;
+                Ok(Self {
+                    grid,
+                    width,
+                    height,
+                })
+            }
+            _ => Err(GridResizeParseError),
+        }
+    }
+}
+
+fn pos_int(value: Value) -> Option<u64> {
+    match value {
+        Value::Integer(n) => n.as_u64(),
+        _ => None,
+    }
+}
+
+#[derive(Debug, Clone, Copy, thiserror::Error)]
+#[error("Failed to parse grid_resize event")]
+struct GridResizeParseError;
