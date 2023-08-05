5163584c8ee6544192d73503e7262750bcf4d6a8
Tim Harding <tim@timharding.co>
2023-08-05 13:11:41 -0700

    Owned font type

--- src/image.rs
+++ src/image.rs
@@ -1,10 +1,10 @@
 use png::Encoder;
 use std::{
     fs::{self, File},
-    io::BufWriter,
+    io::{self, BufWriter},
     path::Path,
 };
-use swash::FontRef;
+use swash::{CacheKey, FontRef};
 
 #[allow(unused)]
 pub fn render() {
@@ -27,12 +27,43 @@ pub fn render() {
     }
 
     let font_path = Path::new(r"/usr/share/fonts/TTF/CaskaydiaCoveNerdFont-Regular.ttf");
-    let font_data = fs::read(&font_path).unwrap();
-    let font = FontRef::from_index(&font_data, 0).unwrap();
-    println!("{}", font.attributes());
-    for string in font.localized_strings() {
-        println!("[{:?}] {}", string.id(), string.to_string());
-    }
+    let font = Font::from_file(&font_path, 0).unwrap();
 
     w.write_image_data(&data).unwrap();
+}
+
+pub struct Font {
+    data: Vec<u8>,
+    offset: u32,
+    key: CacheKey,
+}
+
+impl Font {
+    pub fn from_file(path: &Path, index: usize) -> Result<Self, FontFromFileError> {
+        let data = fs::read(&path)?;
+        let font = FontRef::from_index(&data, index).ok_or(FontFromFileError::Font)?;
+        Ok(Self {
+            offset: font.offset,
+            key: font.key,
+            data,
+        })
+    }
+
+    pub fn as_ref(&self) -> FontRef {
+        // Unlike the FontRef constructors, this does not construct a new key,
+        // enabling performance optimizations and caching mechanisms
+        FontRef {
+            data: &self.data,
+            offset: self.offset,
+            key: self.key,
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum FontFromFileError {
+    #[error("IO: {0}")]
+    Io(#[from] io::Error),
+    #[error("Invalid font data or font index out of range")]
+    Font,
 }
