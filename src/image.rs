5eac159188b0987805c2656af277735131043732
Tim Harding <tim@timharding.co>
2023-08-07 13:36:20 -0700

    Initial font atlas implementation

--- src/image.rs
+++ src/image.rs
@@ -1,3 +1,4 @@
+use crate::util::vec2::Vec2;
 use png::{ColorType, Encoder};
 use std::{
     collections::HashMap,
@@ -6,9 +7,10 @@ use std::{
     path::Path,
 };
 use swash::{
-    scale::{image::Image, Render, ScaleContext, Scaler, Source, StrikeWith},
+    scale::{image::Image, Render, ScaleContext, Source, StrikeWith},
     shape::{Direction, ShapeContext},
     text::Script,
+    zeno::Placement,
     CacheKey, FontRef, GlyphId,
 };
 
@@ -28,66 +30,233 @@ pub fn render() {
         .direction(Direction::RightToLeft)
         .size(24.0)
         .build();
-    shaper.add_str("-> - >");
+    shaper.add_str("Things and stuff");
     let mut scale_context = ScaleContext::new();
     let mut scaler = scale_context
         .builder(cascadia.as_ref())
         .size(24.0)
         .hint(true)
         .build();
-    let mut cache = FontCache::new();
+    let mut cache = FontAtlas::from_font(cascadia.as_ref(), 24.0);
     const WIDTH: usize = 600;
     const HEIGHT: usize = 40;
     let mut data = [0u8; WIDTH * HEIGHT];
     let mut x_offset = 0;
-    shaper.shape_with(|cluster| {
-        for glyph in cluster.glyphs {
-            let image = cache.get(glyph.id, &mut scaler);
-            for y in 0..image.placement.height as i32 {
-                for x in 0..image.placement.width as i32 {
-                    let dst_y = y - image.placement.top - glyph.y as i32 + 24;
-                    let dst_x = x + x_offset + image.placement.left + glyph.x as i32;
-                    if dst_y < 0 || dst_x < 0 {
-                        continue;
-                    }
-                    let dst_i = dst_y as usize * WIDTH + dst_x as usize;
-                    let src_i = y as usize * image.placement.width as usize + x as usize;
-                    data[dst_i] = data[dst_i].saturating_add(image.data[src_i]);
-                }
-            }
-            x_offset += glyph.advance.floor() as i32;
-        }
-    });
-
+    // shaper.shape_with(|cluster| {
+    //     for glyph in cluster.glyphs {
+    //         let image = cache.glyph(glyph.id).unwrap();
+    //         for y in 0..image.placement.height as i32 {
+    //             for x in 0..image.placement.width as i32 {
+    //                 let dst_y = y - image.placement.top - glyph.y as i32 + 24;
+    //                 let dst_x = x + x_offset + image.placement.left + glyph.x as i32;
+    //                 if dst_y < 0 || dst_x < 0 {
+    //                     continue;
+    //                 }
+    //                 let dst_i = dst_y as usize * WIDTH + dst_x as usize;
+    //                 let src_i = y as usize * image.placement.width as usize + x as usize;
+    //                 data[dst_i] = data[dst_i].saturating_add(image.data[src_i]);
+    //             }
+    //         }
+    //         x_offset += glyph.advance.floor() as i32;
+    //     }
+    // });
     write_png(
         "/home/tim/temp.png",
-        WIDTH as u32,
-        HEIGHT as u32,
+        cache.size() as u32,
+        cache.size() as u32,
         ColorType::Grayscale,
-        &data,
+        cache.data(),
     );
 }
 
-#[derive(Clone, Default)]
-pub struct FontCache {
-    glyphs: HashMap<GlyphId, Image>,
+// TODO: u16 overflow handling. What should be the maximum texture size?
+
+// Algorithm borrowed from
+// https://straypixels.net/texture-packing-for-fonts/
+struct FontAtlas {
+    /// x and y dimensions of the texture
+    size: u16,
+    /// Root of the glyph tree
+    root: Node,
+    /// Glyph atlas image data
+    data: Vec<u8>,
+    /// A lookup table from glyphs to their rendering info.
+    lut: HashMap<GlyphId, PackedGlyph>,
 }
 
-impl FontCache {
+impl FontAtlas {
     pub fn new() -> Self {
-        Self::default()
+        const DEFAULT_SIZE: u16 = 256;
+        Self {
+            size: DEFAULT_SIZE,
+            root: Node::new(Vec2::new(0, 0), Vec2::new(u16::MAX, u16::MAX)),
+            data: vec![0u8; DEFAULT_SIZE as usize * DEFAULT_SIZE as usize],
+            lut: HashMap::default(),
+        }
     }
 
-    pub fn get(&mut self, glyph: GlyphId, scaler: &mut Scaler) -> &Image {
-        self.glyphs.entry(glyph).or_insert_with(|| {
-            Render::new(&[
+    pub fn from_font(font: FontRef, size: f32) -> Self {
+        let mut glyphs = vec![];
+        let mut scale_context = ScaleContext::new();
+        let mut scaler = scale_context.builder(font).size(size).hint(true).build();
+        font.charmap().enumerate(|_c, id| {
+            let image = Render::new(&[
                 Source::ColorOutline(0),
                 Source::ColorBitmap(StrikeWith::BestFit),
                 Source::Outline,
             ])
-            .render(scaler, glyph)
-            .unwrap()
-        })
+            .render(&mut scaler, id)
+            .unwrap();
+            glyphs.push((id, image));
+        });
+        glyphs.sort_unstable_by(|(_, l), (_, r)| {
+            let size = |g: &Image| g.placement.width * g.placement.height;
+            size(l).cmp(&size(r))
+        });
+        let mut this = Self::new();
+        for (id, image) in glyphs {
+            this.pack(id, &image);
+        }
+        this
+    }
+
+    pub fn pack(&mut self, id: GlyphId, image: &Image) -> Pack {
+        let mut resized = false;
+        let glyph_size = Vec2::new(image.placement.width as u16, image.placement.height as u16);
+        let origin = if let Some(node) = self.root.pack(glyph_size, self.size) {
+            node
+        } else {
+            resized = true;
+            let old_size = self.size;
+            self.size *= 2;
+            let old = std::mem::take(&mut self.data);
+            self.data = vec![0u8; self.size as usize * self.size as usize];
+            for (src, dst) in old
+                .chunks(old_size as usize)
+                .zip(self.data.chunks_mut(self.size as usize))
+            {
+                for (src, dst) in src.into_iter().zip(dst.into_iter()) {
+                    *dst = *src;
+                }
+            }
+            self.root.pack(glyph_size, self.size).unwrap()
+        };
+
+        for (src, dst) in image
+            .data
+            .chunks(image.placement.width as usize)
+            .skip(origin.y as usize)
+            .zip(self.data.chunks_mut(self.size as usize))
+        {
+            for (src, dst) in src.into_iter().zip(dst.into_iter().skip(origin.x as usize)) {
+                *dst = *src;
+            }
+        }
+
+        self.lut.insert(
+            id,
+            PackedGlyph {
+                origin,
+                placement: image.placement,
+            },
+        );
+
+        Pack { resized, origin }
+    }
+
+    pub fn data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn size(&self) -> u16 {
+        self.size
+    }
+
+    pub fn get(&self, id: GlyphId) -> Option<&PackedGlyph> {
+        self.lut.get(&id)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PackedGlyph {
+    origin: Vec2<u16>,
+    placement: Placement,
+}
+
+pub struct Pack {
+    resized: bool,
+    origin: Vec2<u16>,
+}
+
+struct Node {
+    origin: Vec2<u16>,
+    size: Vec2<u16>,
+    is_filled: bool,
+    children: Option<(Box<Node>, Box<Node>)>,
+}
+
+impl Node {
+    pub fn new(origin: Vec2<u16>, size: Vec2<u16>) -> Self {
+        Self {
+            origin,
+            size,
+            is_filled: false,
+            children: None,
+        }
+    }
+
+    pub fn pack(&mut self, size: Vec2<u16>, texture_size: u16) -> Option<Vec2<u16>> {
+        if self.is_filled {
+            return None;
+        } else if let Some(children) = self.children.as_mut() {
+            children
+                .0
+                .pack(size, texture_size)
+                .or_else(|| children.1.pack(size, texture_size))
+        } else {
+            let real_size = {
+                let mut real_size = self.size;
+                if self.origin.x + self.size.x == u16::MAX {
+                    real_size.x = self.size.x - self.origin.x;
+                }
+                if self.origin.y + self.size.y == u16::MAX {
+                    real_size.y = self.size.y - self.origin.y;
+                }
+                real_size
+            };
+
+            if self.size == size {
+                self.is_filled = true;
+                Some(self.origin)
+            } else if real_size.x < size.x || real_size.y < size.y {
+                None
+            } else {
+                let remainder = real_size - size;
+                let vertical_split = if remainder == Vec2::new(0, 0) {
+                    // If we are going to the edge of the texture, split
+                    // according to the glyph dimensions instead
+                    self.size.x < self.size.y
+                } else {
+                    remainder.x < remainder.y
+                };
+
+                self.children = Some((
+                    Box::new(Node::new(self.origin, self.size)),
+                    Box::new(if vertical_split {
+                        Node::new(
+                            Vec2::new(self.origin.x, self.origin.y + size.y),
+                            Vec2::new(self.size.x, self.size.y - size.y),
+                        )
+                    } else {
+                        Node::new(
+                            Vec2::new(self.origin.x + size.x, self.origin.y),
+                            Vec2::new(self.size.x - size.x, self.size.y),
+                        )
+                    }),
+                ));
+                self.children.as_mut().unwrap().0.pack(size, texture_size)
+            }
+        }
     }
 }
 
--- src/util/vec2.rs
+++ src/util/vec2.rs
@@ -1,33 +1,33 @@
 use std::ops::{Add, Mul, Sub};
 
-pub type Vec2u = Vec2T<u64>;
-pub type Vec2s = Vec2T<usize>;
-pub type Vec2i = Vec2T<i64>;
-pub type Vec2f = Vec2T<f64>;
+pub type Vec2u = Vec2<u64>;
+pub type Vec2s = Vec2<usize>;
+pub type Vec2i = Vec2<i64>;
+pub type Vec2f = Vec2<f64>;
 
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
-pub struct Vec2T<T> {
+pub struct Vec2<T> {
     pub x: T,
     pub y: T,
 }
 
-impl<T> Vec2T<T> {
+impl<T> Vec2<T> {
     pub fn new(x: T, y: T) -> Self {
         Self { x, y }
     }
 }
 
-impl<T> Into<(T, T)> for Vec2T<T> {
+impl<T> Into<(T, T)> for Vec2<T> {
     fn into(self) -> (T, T) {
         (self.x, self.y)
     }
 }
 
-impl<T> Add for Vec2T<T>
+impl<T> Add for Vec2<T>
 where
     T: Add<Output = T>,
 {
-    type Output = Vec2T<T>;
+    type Output = Vec2<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
         Self {
@@ -37,11 +37,11 @@ where
     }
 }
 
-impl<T> Sub for Vec2T<T>
+impl<T> Sub for Vec2<T>
 where
     T: Sub<Output = T>,
 {
-    type Output = Vec2T<T>;
+    type Output = Vec2<T>;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Self {
@@ -51,11 +51,11 @@ where
     }
 }
 
-impl<T> Mul for Vec2T<T>
+impl<T> Mul for Vec2<T>
 where
     T: Mul<Output = T>,
 {
-    type Output = Vec2T<T>;
+    type Output = Vec2<T>;
 
     fn mul(self, rhs: Self) -> Self::Output {
         Self::new(self.x * rhs.x, self.y * rhs.y)
@@ -64,14 +64,14 @@ where
 
 macro_rules! vec_try_from {
     ($t:ty, $u:ty) => {
-        impl TryFrom<Vec2T<$t>> for Vec2T<$u>
+        impl TryFrom<Vec2<$t>> for Vec2<$u>
         where
             $u: TryFrom<$t>,
         {
             type Error = <$u as TryFrom<$t>>::Error;
 
-            fn try_from(value: Vec2T<$t>) -> Result<Self, Self::Error> {
-                Ok(Vec2T::new(value.x.try_into()?, value.y.try_into()?))
+            fn try_from(value: Vec2<$t>) -> Result<Self, Self::Error> {
+                Ok(Vec2::new(value.x.try_into()?, value.y.try_into()?))
             }
         }
     };
