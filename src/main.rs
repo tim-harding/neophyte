1a186bb68a2ab3ed3fbba8337e63beb284e553d0
Tim Harding <tim@timharding.co>
2023-08-23 16:23:55 -0700

    Fixed input handling

--- src/main.rs
+++ src/main.rs
@@ -48,137 +48,170 @@ fn main() {
     }
 
     let mut modifiers = ModifiersState::default();
-    event_loop.run(move |event, _, control_flow| match event {
-        winit::event::Event::WindowEvent {
-            window_id,
-            ref event,
-        } if window_id == window.id() => match event {
-            WindowEvent::ModifiersChanged(new_modifiers) => {
-                modifiers = *new_modifiers;
-            }
-
-            WindowEvent::ReceivedCharacter(c) => {
-                if !c.is_control() && !c.is_whitespace() {
-                    send_keys(&format!("{c}"), &mut modifiers, &mut neovim);
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            winit::event::Event::WindowEvent {
+                window_id,
+                ref event,
+            } if window_id == window.id() => match event {
+                WindowEvent::ModifiersChanged(new_modifiers) => {
+                    modifiers = *new_modifiers;
                 }
-            }
 
-            WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(virtual_keycode),
-                        ..
-                    },
-                ..
-            } => {
-                let c = || {
-                    Some(match virtual_keycode {
-                        VirtualKeyCode::Escape => "Esc",
-                        VirtualKeyCode::F1 => "F1",
-                        VirtualKeyCode::F2 => "F2",
-                        VirtualKeyCode::F3 => "F3",
-                        VirtualKeyCode::F4 => "F4",
-                        VirtualKeyCode::F5 => "F5",
-                        VirtualKeyCode::F6 => "F6",
-                        VirtualKeyCode::F7 => "F7",
-                        VirtualKeyCode::F8 => "F8",
-                        VirtualKeyCode::F9 => "F9",
-                        VirtualKeyCode::F10 => "F10",
-                        VirtualKeyCode::F11 => "F11",
-                        VirtualKeyCode::F12 => "F12",
-                        VirtualKeyCode::F13 => "F13",
-                        VirtualKeyCode::F14 => "F14",
-                        VirtualKeyCode::F15 => "F15",
-                        VirtualKeyCode::F16 => "F16",
-                        VirtualKeyCode::F17 => "F17",
-                        VirtualKeyCode::F18 => "F18",
-                        VirtualKeyCode::F19 => "F19",
-                        VirtualKeyCode::F20 => "F20",
-                        VirtualKeyCode::F21 => "F21",
-                        VirtualKeyCode::F22 => "F22",
-                        VirtualKeyCode::F23 => "F23",
-                        VirtualKeyCode::F24 => "F24",
-                        VirtualKeyCode::Back => "BS",
-                        VirtualKeyCode::Home => "Home",
-                        VirtualKeyCode::Delete => "Del",
-                        VirtualKeyCode::End => "End",
-                        VirtualKeyCode::PageDown => "PageDown",
-                        VirtualKeyCode::PageUp => "PageUp",
-                        VirtualKeyCode::Left => "Left",
-                        VirtualKeyCode::Up => "Up",
-                        VirtualKeyCode::Right => "Right",
-                        VirtualKeyCode::Down => "Down",
-                        VirtualKeyCode::Return => "Enter",
-                        VirtualKeyCode::Space => "Space",
-                        VirtualKeyCode::Numpad0 => "k0",
-                        VirtualKeyCode::Numpad1 => "k1",
-                        VirtualKeyCode::Numpad2 => "k2",
-                        VirtualKeyCode::Numpad3 => "k3",
-                        VirtualKeyCode::Numpad4 => "k4",
-                        VirtualKeyCode::Numpad5 => "k5",
-                        VirtualKeyCode::Numpad6 => "k6",
-                        VirtualKeyCode::Numpad7 => "k7",
-                        VirtualKeyCode::Numpad8 => "k8",
-                        VirtualKeyCode::Numpad9 => "k9",
-                        VirtualKeyCode::NumpadAdd => "kPlus",
-                        VirtualKeyCode::NumpadDivide => "kDivide",
-                        VirtualKeyCode::NumpadDecimal => "kPoint",
-                        VirtualKeyCode::NumpadComma => "kComma",
-                        VirtualKeyCode::NumpadEnter => "kEnter",
-                        VirtualKeyCode::NumpadEquals => "kEqual",
-                        VirtualKeyCode::NumpadMultiply => "kMultiply",
-                        VirtualKeyCode::NumpadSubtract => "kMinus",
-                        VirtualKeyCode::Tab => "Tab",
-                        _ => return None,
-                    })
-                };
-                if let Some(c) = c() {
-                    send_keys(c, &mut modifiers, &mut neovim);
+                WindowEvent::ReceivedCharacter(c) => {
+                    if !c.is_control()
+                        && !c.is_whitespace()
+                        && !c.is_ascii_digit()
+                        && !c.is_ascii_alphabetic()
+                    {
+                        send_keys(&format!("{c}"), &mut modifiers, &mut neovim, true);
+                    }
                 }
-            }
 
-            WindowEvent::Resized(physical_size) => {
-                render_tx
-                    .send(RenderEvent::Resized(*physical_size))
-                    .unwrap();
-            }
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(virtual_keycode),
+                            ..
+                        },
+                    ..
+                } => {
+                    let c = || {
+                        Some(match virtual_keycode {
+                            VirtualKeyCode::Escape => "Esc",
+                            VirtualKeyCode::F1 => "F1",
+                            VirtualKeyCode::F2 => "F2",
+                            VirtualKeyCode::F3 => "F3",
+                            VirtualKeyCode::F4 => "F4",
+                            VirtualKeyCode::F5 => "F5",
+                            VirtualKeyCode::F6 => "F6",
+                            VirtualKeyCode::F7 => "F7",
+                            VirtualKeyCode::F8 => "F8",
+                            VirtualKeyCode::F9 => "F9",
+                            VirtualKeyCode::F10 => "F10",
+                            VirtualKeyCode::F11 => "F11",
+                            VirtualKeyCode::F12 => "F12",
+                            VirtualKeyCode::Back => "BS",
+                            VirtualKeyCode::Home => "Home",
+                            VirtualKeyCode::Delete => "Del",
+                            VirtualKeyCode::End => "End",
+                            VirtualKeyCode::PageDown => "PageDown",
+                            VirtualKeyCode::PageUp => "PageUp",
+                            VirtualKeyCode::Left => "Left",
+                            VirtualKeyCode::Up => "Up",
+                            VirtualKeyCode::Right => "Right",
+                            VirtualKeyCode::Down => "Down",
+                            VirtualKeyCode::Return => "Enter",
+                            VirtualKeyCode::Space => "Space",
+                            VirtualKeyCode::Numpad0 => "k0",
+                            VirtualKeyCode::Numpad1 => "k1",
+                            VirtualKeyCode::Numpad2 => "k2",
+                            VirtualKeyCode::Numpad3 => "k3",
+                            VirtualKeyCode::Numpad4 => "k4",
+                            VirtualKeyCode::Numpad5 => "k5",
+                            VirtualKeyCode::Numpad6 => "k6",
+                            VirtualKeyCode::Numpad7 => "k7",
+                            VirtualKeyCode::Numpad8 => "k8",
+                            VirtualKeyCode::Numpad9 => "k9",
+                            VirtualKeyCode::NumpadAdd => "kPlus",
+                            VirtualKeyCode::NumpadDivide => "kDivide",
+                            VirtualKeyCode::NumpadDecimal => "kPoint",
+                            VirtualKeyCode::NumpadComma => "kComma",
+                            VirtualKeyCode::NumpadEnter => "kEnter",
+                            VirtualKeyCode::NumpadEquals => "kEqual",
+                            VirtualKeyCode::NumpadMultiply => "kMultiply",
+                            VirtualKeyCode::NumpadSubtract => "kMinus",
+                            VirtualKeyCode::Tab => "Tab",
+                            VirtualKeyCode::Key1 => "1",
+                            VirtualKeyCode::Key2 => "2",
+                            VirtualKeyCode::Key3 => "3",
+                            VirtualKeyCode::Key4 => "4",
+                            VirtualKeyCode::Key5 => "5",
+                            VirtualKeyCode::Key6 => "6",
+                            VirtualKeyCode::Key7 => "7",
+                            VirtualKeyCode::Key8 => "8",
+                            VirtualKeyCode::Key9 => "9",
+                            VirtualKeyCode::Key0 => "0",
+                            VirtualKeyCode::A => "a",
+                            VirtualKeyCode::B => "b",
+                            VirtualKeyCode::C => "c",
+                            VirtualKeyCode::D => "d",
+                            VirtualKeyCode::E => "e",
+                            VirtualKeyCode::F => "f",
+                            VirtualKeyCode::G => "g",
+                            VirtualKeyCode::H => "h",
+                            VirtualKeyCode::I => "i",
+                            VirtualKeyCode::J => "j",
+                            VirtualKeyCode::K => "k",
+                            VirtualKeyCode::L => "l",
+                            VirtualKeyCode::M => "m",
+                            VirtualKeyCode::N => "n",
+                            VirtualKeyCode::O => "o",
+                            VirtualKeyCode::P => "p",
+                            VirtualKeyCode::Q => "q",
+                            VirtualKeyCode::R => "r",
+                            VirtualKeyCode::S => "s",
+                            VirtualKeyCode::T => "t",
+                            VirtualKeyCode::U => "u",
+                            VirtualKeyCode::V => "v",
+                            VirtualKeyCode::W => "w",
+                            VirtualKeyCode::X => "x",
+                            VirtualKeyCode::Y => "y",
+                            VirtualKeyCode::Z => "z",
+                            _ => return None,
+                        })
+                    };
+                    if let Some(c) = c() {
+                        send_keys(c, &mut modifiers, &mut neovim, false);
+                    }
+                }
 
-            // TODO: Use the scale factor
-            WindowEvent::ScaleFactorChanged {
-                new_inner_size,
-                scale_factor: _,
-            } => {
-                render_tx
-                    .send(RenderEvent::Resized(**new_inner_size))
-                    .unwrap();
-            }
+                WindowEvent::Resized(physical_size) => {
+                    render_tx
+                        .send(RenderEvent::Resized(*physical_size))
+                        .unwrap();
+                }
 
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                // TODO: Use the scale factor
+                WindowEvent::ScaleFactorChanged {
+                    new_inner_size,
+                    scale_factor: _,
+                } => {
+                    render_tx
+                        .send(RenderEvent::Resized(**new_inner_size))
+                        .unwrap();
+                }
+
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+
+                _ => {}
+            },
+
+            winit::event::Event::RedrawRequested(window_id) if window_id == window.id() => {
+                render_tx.send(RenderEvent::Redraw).unwrap();
+            }
 
             _ => {}
-        },
-
-        winit::event::Event::RedrawRequested(window_id) if window_id == window.id() => {
-            render_tx.send(RenderEvent::Redraw).unwrap();
         }
-
-        _ => {}
     })
 }
 
-fn send_keys(c: &str, modifiers: &mut ModifiersState, neovim: &mut Neovim) {
-    let c = if modifiers.is_empty() {
+fn send_keys(c: &str, modifiers: &mut ModifiersState, neovim: &mut Neovim, ignore_shift: bool) {
+    let shift = modifiers.shift() && !ignore_shift;
+    let c = if modifiers.ctrl() || modifiers.alt() || modifiers.logo() || shift {
+        let ctrl = if modifiers.ctrl() { "C" } else { "" };
+        let shift = if shift { "S" } else { "" };
+        let alt = if modifiers.alt() { "A" } else { "" };
+        let logo = if modifiers.logo() { "D" } else { "" };
+        format!("<{ctrl}{shift}{alt}{logo}-{c}>")
+    } else {
         match c.len() {
             1 => c.to_string(),
             _ => format!("<{c}>"),
         }
-    } else {
-        let ctrl = if modifiers.ctrl() { "C" } else { "" };
-        let shift = if modifiers.shift() { "S" } else { "" };
-        let alt = if modifiers.alt() { "A" } else { "" };
-        let logo = if modifiers.logo() { "D" } else { "" };
-        format!("<{ctrl}{shift}{alt}{logo}-{c}>")
     };
+    println!("{c}");
     neovim.input(c);
 }
