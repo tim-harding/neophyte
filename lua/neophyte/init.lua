ebc315e223eb80def822c5ebac34e68a38116e18
Tim Harding <tim@timharding.co>
2023-10-07 20:40:21 -0700

    Don't allow float font sizes

--- lua/neophyte/init.lua
+++ lua/neophyte/init.lua
@@ -2,12 +2,14 @@ local M = {}
 
 ---@param height number
 function M.set_font_height(height)
-  vim.rpcnotify(1, "neophyte.set_font_height", { height })
+  local integral, _ = math.modf(height)
+  vim.rpcnotify(1, "neophyte.set_font_height", { integral })
 end
 
 ---@param width number
 function M.set_font_width(width)
-  vim.rpcnotify(1, "neophyte.set_font_width", { width })
+  local integral, _ = math.modf(width)
+  vim.rpcnotify(1, "neophyte.set_font_width", { integral })
 end
 
 return M
